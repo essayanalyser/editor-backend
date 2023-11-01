8ca6ef5b369a258fee741393f8c21d1a45b46696
jayneet <jaynitjain639@gmail.com>
2023-11-01 19:02:44 +0530

    format changed

--- backend/editor_users/views.py
+++ backend/editor_users/views.py
@@ -71,6 +71,7 @@ class HistoryDataView(APIView):
             sentences = []
             count = 0
             para = 0
+            detail_versions = []
             # print(detail.data)
             # print(detail)
             detail_content_list = detail.data["content"].split(".")
@@ -90,13 +91,32 @@ class HistoryDataView(APIView):
                 if count == 0:
                     detail_content += [{"para":para,"sentences": sentences}]
                 count+=1
-            
+                
             detail_res += [ {"title": detail.key.title,"doc_name":detail.doc_name,"version": detail.data["version"],"content": detail_content} ]
+        def version_manage(list_of_dict):
+            doc_name_list = []
+            final_data = []
+            versions = []
+            version_list = []
+            version_manage = {}
+            for i in list_of_dict:
+                if i["doc_name"] not in doc_name_list:
+                    doc_name_list.append(i["doc_name"])
+                    version_manage.update({i["doc_name"]:[]})
+                if i["doc_name"] in doc_name_list:
+                    version_list = [{"version":i["version"],"content":i["content"]}]
+                    version_manage[i["doc_name"]] += version_list 
+                # versions += version_list   
+            for i in version_manage:
+                final_data += [{"doc_name": i,"versions":version_manage[i]}]
+            return final_data
+                    
         user_list_data = []
         for detail in detail_res:
             if detail["title"] == key:
                 user_list_data += [{"doc_name":detail["doc_name"],"version":detail["version"],"content":detail["content"]}] 
-        return Response(user_list_data)
+        return Response(version_manage(user_list_data))
+        # return Response(user_list_data)
 
     def post(self,request):
         print(request.data)
